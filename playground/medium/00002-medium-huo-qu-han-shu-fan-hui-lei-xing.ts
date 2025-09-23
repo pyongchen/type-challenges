24390e1cada3120297d27fb804dc9ba065e3b533
chenpy8 <734162700@.qq.com>
2025-09-23 09:42:40 +0800

    feat: medium exerc1

--- playground/medium/00002-medium-huo-qu-han-shu-fan-hui-lei-xing.ts
+++ playground/medium/00002-medium-huo-qu-han-shu-fan-hui-lei-xing.ts
@@ -25,7 +25,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyReturnType<T> = any
+type MyReturnType<T extends (...arg: any) => any> = T extends (...arg: any) => infer Res ? Res : never;
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
--- playground/medium/00008-medium-dui-xiang-bu-fen-shu-xing-zhi-du.ts
+++ playground/medium/00008-medium-dui-xiang-bu-fen-shu-xing-zhi-du.ts
@@ -34,7 +34,11 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MyReadonly2<T, K> = any
+type MyReadonly2<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P];
+} & {
+  [P in keyof T as P extends K? never: P]: T[P]
+}
 
 /* _____________ 测试用例 _____________ */
 import type { Alike, Expect } from '@type-challenges/utils'

daa4f78c21fc4d09c1a1f30cdacea855942fbe44
chenpy8 <734162700@.qq.com>
2025-10-28 00:00:01 +0800

    feat: exercise 251027

--- playground/medium/03062-medium-shift.ts
+++ playground/medium/03062-medium-shift.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Shift<T> = any
+type Shift<T extends unknown[]> = T extends [infer _, ...infer Right] ? Right : []
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
